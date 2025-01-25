14d753ed6cc275662e0813e2377114dfad799cd5
lexQzim <12845370+LexQzim@users.noreply.github.com>
2025-01-25 15:32:54 +0100

    Init

---
+++ src/test/java/de/pottgraben/digiport/DigiportApplicationTests.java
@@ -0,0 +1,13 @@
+package de.pottgraben.digiport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
